d5d9e2c9e7adf0904a0e80f16e5e6a17f0bab29f
inni-e <innie.red@gmail.com>
2023-05-31 14:47:13 +1000

    Co-authored-by: HARI-PRMD <HARI-PRMD@users.noreply.github.com> Co-authored-by: Merry Rosalie <MintyKanna@users.noreply.github.com>

---
+++ src/pages/coursepage.tsx
@@ -0,0 +1,25 @@
+import { NextPage } from "next";
+import Layout from "~/components/Layout";
+
+const CoursePage: NextPage = () => {
+  return (
+    <Layout>
+      <div>
+        {/* Banner */}
+        <div>
+
+        </div>
+
+        {/* Posts section */}
+        <div>
+
+        </div>
+
+        {/* Members section */}
+        <div>
+
+        </div>
+      </div>
+    </Layout>
+  )
+}
